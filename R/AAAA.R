636a91a2304c9981bdfabec331ab868f0a97af08
Rainer M. Krug <Rainer@krugs.de>
2018-09-03 19:18:47 +0200

    Removed API key

--- R/AAAA.R
+++ R/AAAA.R
@@ -2,5 +2,5 @@
 
 .onLoad <- function(lib, pkg) {
   set_option("os_url", "https://api.originstamp.org/api/")
-  set_option("api_key", "51996e47-1260-4c6b-b8ad-bdccdd320a64")
+  set_option("api_key", "Please get valid API key from Originstamp.org!")
 }
