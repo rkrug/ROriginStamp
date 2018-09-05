b4098f4a2b172d5a22cefb1c8b1d199907bbc628
Rainer M. Krug <Rainer@krugs.de>
2018-09-05 14:30:44 +0200

    testthat.R: add automatic local / remote detection to set API key locally

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,16 @@
 library(testthat)
 library(ROriginStamp)
 
-test_check("ROriginStamp")
+if ( Sys.getenv("api_keys") == "" ) {
+  if ( file.exists("./ONLY_LOCAL_NOT_ON_GITHUB.R") ) {
+    source("./ONLY_LOCAL_NOT_ON_GITHUB.R")
+  }
+}
+
+ROriginStamp_options(
+  api_key = Sys.getenv("api_key")
+)
+
+if ( ROriginStamp_options("api_key") != "" ) {
+  test_check("ROriginStamp")
+}
